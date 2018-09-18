59a2ca818b84cab287ba1ddd8476836c39c1d731
Prometei299 <pavel-luchs@ya.ru>
2018-09-18 11:59:20 +0300

    Open and Close Backets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = false, counter = 0, arr = str.split('');
+
+  for (let i = 0; i < bracketsConfig.length; ++i) {
+    let CheckOpenBackets = arr.indexOf(bracketsConfig[i][0]);
+    let CheckCloseBackets = arr.indexOf(bracketsConfig[i][1]);
+
+    if (CheckCloseBackets < CheckOpenBackets) {
+      result = false;
+      break;
+    }
+    for (let j = CheckOpenBackets; j < arr.length; ++j) {
+      if (arr[j] === bracketsConfig[i][0]) {
+        counter++;
+      }
+      if (arr[j] === bracketsConfig[i][1]) {
+        counter--;
+      }
+    }
+    if (!counter) {
+      result = true;
+    } else {
+      result = false;
+    }
+  }
+
+  return result;
 }
